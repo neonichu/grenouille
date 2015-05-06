2a8dd415386d118c2f056467b0bc816d6663b858
Boris Bügling <boris@icculus.org>
2015-05-06 21:26:41 +0200

    Add CircleCI special sauce for Coveralls.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 require 'coveralls'
 Coveralls.wear!
 
+if ENV['CIRCLE_ARTIFACTS']
+  require 'simplecov'
+  dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
 require 'pathname'
 ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 $LOAD_PATH.unshift((ROOT + 'lib').to_s)
